0e04f1c8ffcb1b9b1dc8904d39c62c9fadba5bb6
Pawan <pawanbeniwal012@gmail.com>
2023-12-19 12:39:50 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+import userRoutes from './routes/users.js'
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
+
+const app=express()
+dotenv.config();
+app.use(express.json({limit:"30mb",extended:true}))
+app.use(express.urlencoded({limit:"30mb",extended:true}))
+app.use(cors());
+
+app.get('/',(req,res)=>{ 
+    res.send("This is a stack overflow clone API")
+}) 
+
+app.use('/user',userRoutes)
+app.use('/questions',questionRoutes)
+app.use('/answer',answerRoutes)
+ 
+const PORT=process.env.PORT||5000
+//"mongodb://localhost:27017/Stackoverflow?authMechanism=DEFAULT"
+const DATABASE_URL=process.env.CONNECTION_URL//1
+mongoose.connect(DATABASE_URL)//2
+ app.listen(PORT,()=>{
+    console.log(`app running on http://localhost:${PORT}`)
+ })
